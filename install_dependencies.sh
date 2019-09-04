8f8c0265c9d2657f01ca6417c179f6e07ef6f24b
Vicent Ahuir Esteve <viahes@eui.upv.es>
2019-09-04 14:05:43 +0200

    Install dependencies in a single step

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,12 +1,12 @@
 #!bin/bash
 
-yarn add request
-yarn add cheerio
-yarn add date-utils
-yarn add proxy-agent
-yarn add iconv-lite
-yarn add charset-parser
-yarn add file-state-monitor
-yarn add mongodb
-yarn add express
-yarn add circular-buffer
+yarn add request \
+	cheerio \
+	date-utils \
+	proxy-agent \
+	iconv-lite \
+	charset-parser \
+	file-state-monitor \
+	mongodb \
+	express \
+	circular-buffer
